277919f2a543001a00dcadbffff3cfc1e425d8ca
Lars Kiesow <lkiesow@uos.de>
2019-02-10 22:49:53 +0100

    MH-13356, Introduce native existence check
    This patch introduces a native query as a fast check of the existence of
    any snapshot for a given media package identifier instead of linking
    this check to a sub-query looking for the latest snapshot of a given
    media package.

--- modules/asset-manager-impl/src/main/java/org/opencastproject/assetmanager/impl/AssetManagerWithSecurity.java
+++ modules/asset-manager-impl/src/main/java/org/opencastproject/assetmanager/impl/AssetManagerWithSecurity.java
@@ -33,7 +33,6 @@ import org.opencastproject.assetmanager.api.Value;
 import org.opencastproject.assetmanager.api.Version;
 import org.opencastproject.assetmanager.api.query.ADeleteQuery;
 import org.opencastproject.assetmanager.api.query.AQueryBuilder;
-import org.opencastproject.assetmanager.api.query.AResult;
 import org.opencastproject.assetmanager.api.query.ASelectQuery;
 import org.opencastproject.assetmanager.api.query.Predicate;
 import org.opencastproject.assetmanager.api.query.PropertyField;
@@ -80,19 +79,16 @@ public class AssetManagerWithSecurity extends AssetManagerDecorator<TieredStorag
   @Override public Snapshot takeSnapshot(String owner, MediaPackage mp) {
 
     final String mediaPackageId = mp.getIdentifier().toString();
-    final AQueryBuilder q = q();
-    final AResult r = q.select(q.snapshot())
-            .where(q.mediaPackageId(mediaPackageId).and(q.version().isLatest()))
-           .run();
+    final boolean firstSnapshot = !snapshotExists(mediaPackageId);
 
     // Allow this if:
     //  - no previous snapshot exists
     //  - the user has write access to the previous snapshot
-    if (r.getSize() < 1) {
+    if (firstSnapshot) {
       // if it's the first snapshot, ensure that old, leftover properties are removed
       deleteProperties(mediaPackageId);
     }
-    if (r.getSize() < 1 || isAuthorized(mediaPackageId, WRITE_ACTION)) {
+    if (firstSnapshot || isAuthorized(mediaPackageId, WRITE_ACTION)) {
       final Snapshot snapshot = super.takeSnapshot(owner, mp);
       final AccessControlList acl = authSvc.getActiveAcl(mp).getA();
       storeAclAsProperties(snapshot, acl);
