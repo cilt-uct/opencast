8f32a1869249e24e5c1944a28766549c890cfd69
Nosipho Mali <yheyhePrec@gmail.com>
2021-05-14 09:01:05 +0200

    OPENCAST-3036: Updated metadata event and added confirmation bootstrap modal

--- modules/lti/src/main/resources/tools/settings/js/settings.js
+++ modules/lti/src/main/resources/tools/settings/js/settings.js
@@ -2,20 +2,65 @@ const urlParams = new URLSearchParams(window.location.search);
 const seriesID = urlParams.get('sid');
 
 $(document).ready(function(){
+    var seriesInfo = getSeries("/api/series/" + seriesID + "/metadata");
+    var dublin = seriesInfo[0].fields;
+    var ext = seriesInfo[1].fields;
 
-    getSeriesInfo();
+    for (var x=0; x < dublin.length; x++) {
+        if(dublin[x].id == "title") {
+            $('#series_title').val(dublin[x].value);
+        }
+        if(dublin[x].id == "createdBy") {
+            $('#series_creator').val(dublin[x].value);
+        }
+    }
+
+    for (var j=0; j < ext.length; j++) {
+        if(ext[j].id == "retention-cycle") {
+            if(ext[j].value == "normal") {
+                $('#series_retention  option[value=normal]').attr('selected','selected');
+            } else if(ext[j].value == "long") {
+                $('#series_retention  option[value=long]').attr('selected','selected');
+            } else if(ext[j].value == "forever") {
+                $('#series_retention  option[value=forever]').attr('selected','selected');
+            }
+        }
+        if(ext[j].id == "caption-type") {
+            if(ext[j].value == "none") {
+                $('#series_captions  option[value=none]').attr('selected','selected');
+            } else if(ext[j].value == "google") {
+                $('#series_captions  option[value=google]').attr('selected','selected');
+            } else if(ext[j].value == "") {
+                $('#series_captions  option[value=no_selection]').attr('selected','selected');
+            }
+        }
+        if(ext[j].id == "series-locked") {
+            if(ext[j].value == false) {
+                $('#locked_status').val("No");
+            } else {
+                $('#locked_status').val("Yes");
+            }
+        }
+        if(ext[j].id == "series-expiry-date") {
+            if(ext[j].value != '') {
+                $('#retain_date').val(ext[j].value);
+            } else {
+                $('#retain_date').val('');
+            }
+        }
+    }
 
     $("#settingsForm").submit(function(e){
       e.preventDefault();
       var captions = $('#series_captions').val();
       var fd;
       fd = new FormData();
-      var metadata = {"fields":[{"id":"caption-type","label":"Captions","type":"text","value":captions}]};
-      var captions_data = JSON.stringify(metadata);
+      const newExt = ext.map(obj => obj.id === "caption-type" ? { ...obj, value: captions } : obj)
+      const updatedMetadata = seriesInfo.map(obj => obj.flavor === "ext/series" ? { ...obj, fields: newExt} : obj)
 
-      fd.append("type", "ext/series");
-      fd.append("metadata",captions_data);
-      var url = "/api/series/" + seriesID + "/metadata";
+      var metadata = JSON.stringify(updatedMetadata);
+      fd.append("metadata",metadata);
+      var url = "/api/series/" + seriesID;
 
       $.ajax({
           type: "PUT",
@@ -25,66 +70,22 @@ $(document).ready(function(){
           data: fd,
           dataType: "json"
       }).done(function (data) {
-          console.log(data);
+          $('#confirmationModal').modal('toggle');
       }).fail(function (error) {
           console.log('FAIL');
       });
- });
+    });
 });
 
-function getSeriesInfo() {
-    var ext_url = "/api/series/" + seriesID + "/metadata?type=ext/series",
-    dublincore_url = "/api/series/" + seriesID + "/metadata?type=dublincore/series";
-
-    $.get({url: dublincore_url, responseType: 'json'},
-       function(result) {
-          for (var j=0; j < result.length; j++) {
-              if(result[j].id == "title") {
-                $('#series_title').val(result[j].value);
-              }
-              if(result[j].id == "createdBy") {
-                $('#series_creator').val(result[j].value);
-              }
-          }
-       }
-    )
-
-    $.get({url: ext_url, responseType: 'json'},
-        function(response) {
-           for (var i = 0; i<response.length;i++){
-              if(response[i].id == "retention-cycle") {
-                  if(response[i].value == "normal") {
-                   $('#series_retention  option[value=normal]').attr('selected','selected');
-                  } else if(response[i].value == "long") {
-                   $('#series_retention  option[value=long]').attr('selected','selected');
-                  } else if(response[i].value == "forever") {
-                   $('#series_retention  option[value=forever]').attr('selected','selected');
-                  }
-              }
-              if(response[i].id == "caption-type") {
-                 if(response[i].value == "none") {
-                    $('#series_captions  option[value=none]').attr('selected','selected');
-                 } else if(response[i].value == "google") {
-                    $('#series_captions  option[value=google]').attr('selected','selected');
-                 } else if(response[i].value == "") {
-                    $('#series_captions  option[value=no_selection]').attr('selected','selected');
-                 }
-              }
-              if(response[i].id == "series-locked") {
-                if(response[i].value == false) {
-                  $('#locked_status').val("No");
-                } else {
-                  $('#locked_status').val("Yes");
-                }
-              }
-              if(response[i].id == "series-expiry-date") {
-                 if(response[i].value != '') {
-                   $('#retain_date').val(response[i].value);
-                 } else {
-                   $('#retain_date').val('');
-                 }
-              }
-           }
+function getSeries(url) {
+    return JSON.parse($.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'json',
+        global: false,
+        async: false,
+        success: function (data) {
+           return data;
         }
-    )
+    }).responseText);
 }
