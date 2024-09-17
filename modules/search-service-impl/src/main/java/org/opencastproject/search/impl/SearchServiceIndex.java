f795d47d815e8058f6672f723012e8ff048b4080
Lars Kiesow <lkiesow@uos.de>
2024-09-17 20:46:40 +0200

    Prevent broken XML from failing index rebuild completely
    We had one broken DC XML file which caused a RuntimeException when
    Opencast tried parsing it during the search service search index
    rebuild. This patch prevents the error from creaking the rebuild, only
    logging the error instead.

--- modules/search-service-impl/src/main/java/org/opencastproject/search/impl/SearchServiceIndex.java
+++ modules/search-service-impl/src/main/java/org/opencastproject/search/impl/SearchServiceIndex.java
@@ -355,11 +355,7 @@ public final class SearchServiceIndex extends AbstractIndexProducer implements I
       var updateRequst = new UpdateRequest(INDEX_NAME, mediaPackageId)
           .doc(gson.toJson(json), XContentType.JSON);
       esIndex.getClient().update(updateRequst, RequestOptions.DEFAULT);
-    } catch (ElasticsearchStatusException e) {
-      if (e.status().getStatus() != RestStatus.NOT_FOUND.getStatus()) {
-        throw e;
-      }
-      logger.warn("Event {} is not in the search index. Skipping deletion", mediaPackageId);
+
     } catch (IOException e) {
       throw new SearchException("Could not delete episode " + mediaPackageId + " from index", e);
     }
@@ -473,10 +469,12 @@ public final class SearchServiceIndex extends AbstractIndexProducer implements I
 
             current.getAndIncrement();
             indexMediaPackage(mediaPackage, acl, modificationDate, deletionDate);
-          } catch (SearchServiceDatabaseException | UnauthorizedException | NotFoundException e) {
+          } catch (SearchServiceDatabaseException | UnauthorizedException e) {
             logIndexRebuildError(logger, total, current.get(), e);
             //NB: Runtime exception thrown to escape the functional interfacing
             throw new RuntimeException("Internal Index Rebuild Failure", e);
+          } catch (RuntimeException | NotFoundException e) {
+            logSkippingElement(logger, "event", tuple.getA().getIdentifier().toString(), e);
           }
         });
         //Current is the *page* index, so we remove one since each page only has pageSize entries
