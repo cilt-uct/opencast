65b199f559c98abcf5c1037ace49cd84ad55c6a6
Nosipho Mali <yheyhePrec@gmail.com>
2025-02-12 13:51:11 +0200

    OPENCAST-3273: Ensure transcriptionTypes defaults to 'transcript' when empty

--- modules/engage-paella-player-7/src/plugins/org.opencast.paella.transcriptionPlugin.js
+++ modules/engage-paella-player-7/src/plugins/org.opencast.paella.transcriptionPlugin.js
@@ -67,7 +67,11 @@ export default class transcriptionPlugin extends PopUpButtonPlugin {
       attachments.find(att => ['captions/vtt', 'captions/vtt+en-us'].includes(att.type) && att.mimetype === 'text/vtt');
     this._captionsJsonAttachment = attachments.find(att => att.type === 'captions/json'
       && att.mimetype === 'application/json');
-    this._transcriptionTypes = this._seriesData.find(field => field.id === 'transcript-features')?.value || [];
+    this._transcriptionTypes = this._seriesData.find(field => field.id === 'transcript-features')?.value;
+
+    if (!Array.isArray(this._transcriptionTypes) || this._transcriptionTypes.length === 0) {
+      this._transcriptionTypes = ['transcript'];
+    }
 
     if (this._captionsJsonAttachment) {
       const transcriptResponse = await fetch(this._captionsJsonAttachment.url);
