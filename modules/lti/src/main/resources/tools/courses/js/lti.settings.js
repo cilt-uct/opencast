815563318303d1bdc299bc2d6b64026338a7bcc1
Nosipho Mali <yheyhePrec@gmail.com>
2025-02-15 07:58:24 +0200

    Opencast 3273 transcription plugin for way with words ai generated resources in paella player (#423)
    * OPENCAST-3273: Create transcription plugin and css, add icons and enable in paella player config
    
    * OPENCAST-3273: Disable findCaptionsPlugin
    
    * OPENCAST-3273: Make display options configurable  per series
    
    - Add a configuration field "AI Features" for display options in LTI Settings UI, for series metadata management
    - Read series metadata in paella player plugin
    - hide/show each transcription resource type based on configuration value
    - enable the transcription option  by default and make it non-editable in the LTI UI
    
    * OPENCAST-3273: Add missing semicolon and correct reference to this.transcriptionTypes
    
    * OPENCAST-3273: Improve Transcription Plugin UI
    - Moved plugin button to the bottom right for better accessibility
    - Removed external scrolling outside the transcript
    - Fixed scrolling behavior to ensure smoother interaction
    - Prevented the search feature from interfering with auto-scrolling in the transcript tab
    - Made icons always visible for better usability
    - Resized icons to match the player's icon size for consistency
    - Shortened the AI disclaimer text and adjusted its size for improved readability
    
    * OPENCAST-3273: Update Transcription Type Property and Reorder Settings Form Fields on LTI
    
    * OPENCAST-3273: Ensure transcriptionTypes defaults to 'transcript' when empty
    
    * OPENCAST-3273: Clean Code
    
    * OPENCAST-3273: Fix Github auto build checks issues
    
    * OPENCAST-3273: Fix Github auto build checks issues
    
    * OPENCAST-3273: Fix Guthub auto check issues
    
    * OPENCAST-3273: Try build with Ubuntu 22.04
    
    * OPENCAST-3273: Preserve white spaces in multi-line captions on transcript search and adjust tab-button padding
    
    ---------
    
    Co-authored-by: Corné Oosthuizen <lightscape2001@gmail.com>

---
+++ modules/lti/src/main/resources/tools/courses/js/lti.settings.js
@@ -0,0 +1,163 @@
+const urlParams = new URLSearchParams(window.location.search);
+const seriesID = urlParams.get('sid');
+
+$(document).ready(function(){
+    var seriesInfo = getSeries("/api/series/" + seriesID + "/metadata");
+    var dublin = seriesInfo[0].fields;
+    var ext = seriesInfo[1].fields;
+    const transcriptFeaturesField = document.getElementById('transcript_features');
+    const transcriptFeaturesCheckboxes = document.querySelectorAll('#transcript_features_options .form-check-input');
+
+    // Update transcript-features hidden input when checkboxes are checked/unchecked
+    transcriptFeaturesCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', () => {
+            const selectedFeatures = Array.from(transcriptFeaturesCheckboxes)
+                .filter(cb => cb.checked)
+                .map(cb => cb.value);
+            transcriptFeaturesField.value = selectedFeatures.join(',');
+       });
+    });
+
+
+    for (var x=0; x < dublin.length; x++) {
+        if(dublin[x].id == "title") {
+            $('#series_title').val(dublin[x].value);
+        }
+        if(dublin[x].id == "createdBy") {
+            $('#series_creator').val(dublin[x].value);
+        }
+    }
+
+    for (var j=0; j < ext.length; j++) {
+        if(ext[j].id == "retention-cycle") {
+            if(ext[j].value == "normal" || ext[j].value == "") {
+                $('#series_retention  option[value=normal]').attr('selected','selected');
+            } else if(ext[j].value == "long") {
+                $('#series_retention  option[value=long]').attr('selected','selected');
+            } else if(ext[j].value == "forever") {
+                $('#series_retention  option[value=forever]').attr('selected','selected');
+            }
+        }
+        if(ext[j].id == "caption-type") {
+            if(ext[j].value == "none") {
+                $('#series_captions  option[value=none]').attr('selected','selected');
+            } else if(ext[j].value == "google") {
+                $('#series_captions  option[value=google]').attr('selected','selected');
+            } else if(ext[j].value == "") {
+                $('#series_captions  option[value=no_selection]').attr('selected','selected');
+            }
+        }
+        if(ext[j].id == "series-locked") {
+            if(ext[j].value == false) {
+                $('#locked_status').val("No");
+            } else {
+                $('#locked_status').val("Yes");
+            }
+        }
+        if(ext[j].id == "series-expiry-date") {
+            if(ext[j].value != '') {
+                $('#retain_date').val(ext[j].value);
+            } else {
+                $('#retain_date').val('');
+            }
+        }
+        if(ext[j].id == "notification-list") {
+            if(ext[j].value != '') {
+                $('#notification_list').val(ext[j].value);
+            } else {
+                $('#notification_list').val('');
+            }
+        }
+        if (ext[j].id == "transcript-features") {
+            if (ext[j].value != '') {
+                $('#transcript_features').val(ext[j].value);
+            } else {
+                $('#transcript_features').val('');
+            }
+        }
+    }
+
+    // Pre-check transcription checkboxes based on hidden input value
+    const transcriptFeatures = transcriptFeaturesField.value.split(',').map(feature => feature.trim());
+    // Always show/select transcript
+    if (!transcriptFeatures.includes('transcript')) {
+        transcriptFeatures.push('transcript');
+        transcriptFeaturesField.value = transcriptFeatures.join(',');
+    }
+
+    transcriptFeaturesCheckboxes.forEach(checkbox => {
+        if (transcriptFeatures.includes(checkbox.value)) {
+            checkbox.checked = true;
+        }
+    });
+
+    $("#save_button").click(function(e) {
+        e.preventDefault();
+        var captions = $('#series_captions').val();
+        var retention = $('#series_retention').val();
+        var notificationList = $('#notification_list').val();
+        var transcriptFeatures = $('#transcript_features').val();
+
+        var notificationListArray = notificationList.split(';').map(email => email.trim()).filter(email => email.length > 0);
+        var transcriptFeaturesArray = transcriptFeatures.split(',').map(feature => feature.trim()).filter(feature => feature.length > 0);
+
+        var fd = new FormData();
+        const newExt = ext.map(obj => {
+            switch (obj.id) {
+                case "caption-type":
+                    return { ...obj, value: captions };
+                case "retention-cycle":
+                    return { ...obj, value: retention };
+                case "notification-list":
+                    return { ...obj, value: notificationListArray };
+                case "transcript-features":
+                    return { ...obj, value: transcriptFeaturesArray};
+                default:
+                    return obj;
+            }
+        });
+
+        const updatedMetadata = seriesInfo.map(obj => obj.flavor === "ext/series" ? { ...obj, fields: newExt } : obj);
+
+        var metadata = JSON.stringify(updatedMetadata);
+        fd.append("metadata", metadata);
+        $("#processingModal").modal('show');
+        updateSeriesMetadata(fd);
+    });
+});
+
+function getSeries(url) {
+    return JSON.parse($.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'json',
+        global: false,
+        async: false,
+        success: function (data) {
+           return data;
+        }
+    }).responseText);
+}
+
+function updateSeriesMetadata(fd) {
+    var url = "/api/series/" + seriesID;
+
+    $.ajax({
+        type: "PUT",
+        url: url,
+        processData: false,
+        contentType: false,
+        data: fd,
+        dataType: "json"
+    }).done(function (data) {
+        $("#processingContent").hide();
+        $("#resultMessage").text("Series settings updated successfully!");
+        $("#resultContent").show();
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        $("#processingContent").hide();
+        $("#resultMessage").text("Failed to update series settings. Please try again.");
+        $("#resultContent").show();
+        console.error("Update failed:", textStatus, errorThrown);
+        console.log("Response Text:", jqXHR.responseText);
+    });
+}
